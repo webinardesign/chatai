87b1458e0d2c52b83dd7b081c9d8e18283633e3e
webinardesign <webinardesign1@gmail.com>
2025-04-29 08:38:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,7 @@ import Team from './pages/Team';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="chatgpt" element={<ChatGPT />} />
-        <Route path="team" element={<Team />} />
-      </Route>
+ 
     </Routes>
   );
 }
