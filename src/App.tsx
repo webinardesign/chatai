669c069004dc7b7ea4e8ea9b2e86ed3fdfedc454
webinardesign <webinardesign1@gmail.com>
2025-05-27 03:05:30 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import ChatGPTS from './pages/ChatGPT';
+import ChatGPT from './pages/ChatGPT';
 import Team from './pages/Team';
 function App() {
   return (
